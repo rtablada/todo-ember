925c01bba049443986281d170be0be719fc5c4b1
Ryan Tablada <ryan.tablada@gmail.com>
2017-05-31 17:03:17 -0500

    feat: basic wack a mole

--- app/controllers/wack-a-div.js
+++ app/controllers/wack-a-div.js
@@ -1,12 +1,43 @@
 import Ember from 'ember';
+import {task, timeout} from 'ember-concurrency';
 
 export default Ember.Controller.extend({
   board: null,
+  score: 0,
 
   reset() {
     const board = new Array(16);
-    board.fill(0);
+    board.fill(false);
 
     this.set('board', board);
+    this.set('score', 0);
+  },
+
+  game: task(function* () {
+    while (true) {
+      yield timeout(1000);
+
+      this.changeBoard();
+    }
+  }),
+
+  changeBoard() {
+    const space = Math.floor(Math.random() * 16);
+
+    this.set('board', this.board.map((_, index) => space === index));
+  },
+
+  actions: {
+    startGame() {
+      this.get('game').perform();
+    },
+
+    score(isActive) {
+      if (isActive) {
+        this.set('score', this.score + 1);
+
+        this.changeBoard();
+      }
+    }
   }
 });
